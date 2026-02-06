9239e71880b09868943c9d96257bf4921888cd99
Didi Mardiansyah <148187483+didimardiansyah@users.noreply.github.com>
2026-02-06 14:56:49 +0700

    belun lunas

--- app/pembayaran/page.js
+++ app/pembayaran/page.js
@@ -152,7 +152,7 @@ export default function Pembayaran() {
               <span className="text-sm">
                 {a.persen >= 100
                   ? "✅ Lunas"
-                  : "⏳ Belum Lunas"}
+                  : "⏳ Rung Lunas Cok!"}
               </span>
             </div>
 
